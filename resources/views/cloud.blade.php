a8ec7bdaf13f208f6532c4f659b62048db5969f6
karthick1R <karthick.nexevo@gmail.com>
2026-01-03 16:17:17 +0530

    first commit

---
+++ resources/views/cloud.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/cloud.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cloud Laravel</title>
+</head>
+<body>
+    <h1>Hello Cloud Laravel ☁️</h1>
+    <p>This app is deployed using Cloud Laravel (no cPanel).</p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/cloud', function () {
+    return view('cloud');
+});
+
